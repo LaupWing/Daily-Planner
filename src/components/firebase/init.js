9c1c16a10873a0222227bcc4f2170c29951b411e
LaupWing <loc_strict@hotmail.com>
2019-08-18 22:01:58 +0200

    update

---
+++ src/components/firebase/init.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+var firebaseConfig = {
+    apiKey: "AIzaSyDTf5mLqKAd04TT_EtTM244aWkXkjgIQFU",
+    authDomain: "daily-planner-40cbf.firebaseapp.com",
+    databaseURL: "https://daily-planner-40cbf.firebaseio.com",
+    projectId: "daily-planner-40cbf",
+    storageBucket: "daily-planner-40cbf.appspot.com",
+    messagingSenderId: "307529607050",
+    appId: "1:307529607050:web:bb6a48978d7f0e11"
+  };
+  // Initialize Firebase
+const firebaseApp =  firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore()
