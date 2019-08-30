b61700fe44c0f1638862b3c64dbbe0a5499067f8
LaupWing <loc_strict@hotmail.com>
2019-08-30 16:56:02 +0200

    trying to add skycons

--- src/components/Temperature.vue
+++ src/components/Temperature.vue
@@ -1,20 +1,21 @@
 <template>
-    <div id="Temperature">
-
+    <div id="Temperature" @click="check">
+        {{temperature}}
+        {{summary}}
     </div>
 </template>
 
 <script>
-
+import Skycons from '@/components/helpers/skycons'
 export default {
     name: 'Temperarture',
     props:['weatherData'],
     data(){
         return{
-            temperature: null,
-            summary: null,
-            icon: null,
-            timezone: null
+            temperature:  null,
+            summary:  null,
+            icon:  null,
+            timezone:  null
         }
     },
     methods:{
@@ -22,8 +23,11 @@ export default {
             valNum = parseFloat(valNum)
             return (valNum-32) / 1.8
         },
-        getIcons(){
-            
+        check(){
+            console.log(this.icon)
+        },
+        getIcon(){
+            // const skycon = new Skycons
         }
     },
     watch:{
@@ -32,15 +36,18 @@ export default {
                 this.temperature = Math.round(this.fromFahrenheitToCelcius(this.weatherData.currently.temperature))
                 this.timezone = this.weatherData.timezone
                 this.summary = this.weatherData.currently.summary
-                this.getIcons()
+                this.icon = this.weatherData.currently.icon
+                this.getIcon()
             }
         }
-    },
-    mounted(){
     }
 }
 </script>
 
 <style>
-
+#Temperature{
+    position: fixed;
+    top: 10px;
+    left: 10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import firebase from 'firebase'
-
 Vue.config.productionTip = false
 let app = null
 // ################ What is this? ################
@@ -11,6 +10,7 @@ let app = null
 // Because it takes time for the firebaseapp to initalize to see if there is an user currently logged in
 // Otherwise we will get a null if we want to see if a user is currently logged in
 // The app will only start if the firebaseapp noticed if a user is logged in or not
+
 firebase.auth().onAuthStateChanged(()=>{
   if(!app){
     app = new Vue({
