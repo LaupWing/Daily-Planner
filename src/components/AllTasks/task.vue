91a37dc3660cb6d92337fa71cf3765717d8aa09b
LaupWing <loc_strict@hotmail.com>
2019-09-03 13:32:55 +0200

    refactoring the data

--- src/components/AllTasks/task.vue
+++ src/components/AllTasks/task.vue
@@ -3,6 +3,16 @@
         <h2 :style="{background: task.color.color}">
             {{task.task}}
         </h2>
+        <div class="days">
+            <!-- <p class="day" v-for="(day,index) in task.days" :key="index">{{day.substring(0,3)}}</p> -->
+            <p class="day" :class="{'active':activeDay('mon')}">Mon</p>
+            <p class="day" :class="{'active':activeDay('tue')}">Tue</p>
+            <p class="day" :class="{'active':activeDay('wed')}">Wed</p>
+            <p class="day" :class="{'active':activeDay('thu')}">Thu</p>
+            <p class="day" :class="{'active':activeDay('fri')}">Fri</p>
+            <p class="day" :class="{'active':activeDay('sat')}">Sat</p>
+            <p class="day" :class="{'active':activeDay('sun')}">Sun</p>
+        </div>
     </div>
 </template>
 
@@ -16,6 +26,14 @@ export default {
             const rgbaArray = colorString.get.rgb(this.task.color.color.toLowerCase())
             return {
                 background: `rgba(${rgbaArray[0]},${rgbaArray[1]},${rgbaArray[2]},.2)`
+            }
+        },
+        activeDay(day){
+            const findDay = this.task.days.find(d=>d.substring(0,3).toLowerCase()===day)
+            if(findDay){
+                return true
+            }else{
+                return false
             }
         }
     }
@@ -32,5 +50,19 @@ export default {
     font-size: 1em;
     font-weight: normal;
     padding: 5px;
+}
+.task .days{
+    display: flex;
+    padding: 10px;
+}
+.task .days .day{
+    font-size: .8em;
+    padding: 3px 10px;
+    border: white solid 1px;
+    margin: 0 5px;
+}
+.task .days .day.active{
+    color: black;
+    background: white;
 }
 </style>
