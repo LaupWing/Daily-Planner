a390c8b8f9c78ff4288cdb0239507edabac9832f
LaupWing <loc_strict@hotmail.com>
2019-09-04 13:40:24 +0200

    Popup working

--- src/components/Planner/AddTask/Days/TimePopup.vue
+++ src/components/Planner/AddTask/Days/TimePopup.vue
@@ -135,6 +135,8 @@ export default {
     height: 100vw;
     background: rgba(0,0,0,.4);
     position: fixed;
+    top: 0;
+    bottom: 0;
     left: 0;
     display: flex;
     justify-content: center;
