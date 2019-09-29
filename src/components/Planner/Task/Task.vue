91690ab5cad0e6927d5894fbbe509f4bd46e2fde
LaupWing <loc_strict@hotmail.com>
2019-09-29 15:36:32 +0200

    added comments

---
+++ src/components/Planner/Task/Task.vue
@@ -0,0 +1,16 @@
+<template>
+<!-- need to migrate the code to here  -->
+    <div class="task">
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
