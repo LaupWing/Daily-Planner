a1bea7f93a4b3a46fdfd597686c9c56f2ee3ef27
LaupWing <loc_strict@hotmail.com>
2019-10-22 15:06:34 +0200

    setup tasks.js dailytasks

--- src/store/modules/tasks.js
+++ src/store/modules/tasks.js
@@ -1,17 +1,39 @@
+import firebase from 'firebase'
+import db from '@/firebase/init.js'
+
 const state = {
-    dailyTasks:[]
+    dailyTasks:[],
+    currentUser: null
 }
 
 const getters = {
-
+    getDailyTasks: state => state.dailyTasks,
+    getUsers: state => state.currentUser
 }
 
 const actions = {
+    async fetchDailyTasks({commit}){
+        const doc  = await db
+            .collection('planner')
+            .doc(state.currentUser.uid)
+            .get()
+        if(doc.exists){
+            if(doc.data().dailyTasks){
+                return commit('setDailyTasks', doc.data().dailyTasks)
+            }
+        }
+        commit('setDailyTasks', [])
 
+    },
+    fetchUser({commit}){
+        const user = firebase.auth().currentUser
+        commit('setUser', user)
+    }
 }
 
 const mutations = {
-
+    setUser: (state,user) => (state.currentUser = user),
+    setDailyTasks: (state, dailyTasks)=> (state.dailyTasks = dailyTasks)
 }
 
 export default {
