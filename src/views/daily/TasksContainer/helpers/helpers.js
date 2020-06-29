b94dfa3a7579a60d901f89898caea320226e7244
LaupWing <loc_strict@hotmail.com>
2020-06-29 12:23:39 +0200

    finalize the getClosestCoords

--- src/views/daily/TasksContainer/helpers/helpers.js
+++ src/views/daily/TasksContainer/helpers/helpers.js
@@ -1,6 +1,29 @@
 export function fiveMinuteCoords(){
     const lis = document.querySelectorAll('#Timeline li')
     const coords = []
+
+    const create3MinuteBefore = (li, hour, minute, fiveMinuteCoord)=>{
+        for(let x=3; x>0; x--){
+            const newMinute = (minute - (5*x)) < 0 ?
+                60 - (5*x) : minute - (5*x)
+            const newHour = (minute - (5*x)) < 0 ?
+                hour - 1 : hour
+            coords.push({
+                time: `${newHour < 10 ? '0'+ newHour: newHour}:${newMinute < 10 ? '0'+ newMinute: newMinute}`,
+                coord: (li.offsetTop + (li.offsetHeight / 2)) - fiveMinuteCoord * x
+            })
+        }
+    }
+    const create2MinuteAfter = (li, hour, minute, fiveMinuteCoord)=>{
+        for(let x=1; x<=2; x++){
+            const newMinute = minute + (5*x)
+            coords.push({
+                time: `${hour < 10 ? '0'+ hour: hour}:${newMinute < 10 ? '0'+ newMinute: newMinute}`,
+                coord: (li.offsetTop + (li.offsetHeight / 2)) + fiveMinuteCoord * x
+            })
+        }
+    }
+
     lis.forEach((li,i)=>{
         const minuteCoord = li.offsetHeight / 30
         const hour = Number(li.textContent.includes(':') 
@@ -15,35 +38,22 @@ export function fiveMinuteCoords(){
                 time: li.textContent,
                 coord:li.offsetTop + (li.offsetHeight / 2)
             })
-            for(let x=1; x<=2; x++){
-                const newMinute = minute + (5*x)
-                coords.push({
-                    time: `${hour < 10 ? '0'+ hour: hour}:${newMinute < 10 ? '0'+ newMinute: newMinute}`,
-                    coord: (li.offsetTop + (li.offsetHeight / 2)) + fiveMinuteCoord * x
-                })
-            }
-        }else{
-            for(let x=3; x>0; x--){
-                const newMinute = (minute - (5*x)) < 0 ?
-                    60 - (5*x) : minute - (5*x)
-                const newHour = (minute - (5*x)) < 0 ?
-                    hour - 1 : hour
-                coords.push({
-                    time: `${newHour < 10 ? '0'+ newHour: newHour}:${newMinute < 10 ? '0'+ newMinute: newMinute}`,
-                    coord: (li.offsetTop + (li.offsetHeight / 2)) - fiveMinuteCoord * x
-                })
-            }
+            create2MinuteAfter(li, hour, minute, fiveMinuteCoord)
+        }
+        else if(i=== (lis.length-1)){
+            create3MinuteBefore(li, 24, minute, fiveMinuteCoord)
             coords.push({
                 time: `${hour < 10 ? '0'+ hour: hour}:${minute < 10 ? '0'+ minute: minute}`,
                 coord: (li.offsetTop + (li.offsetHeight / 2))
             })
-            for(let x=1; x<=2; x++){
-                const newMinute = minute + (5*x)
-                coords.push({
-                    time: `${hour < 10 ? '0'+ hour: hour}:${newMinute < 10 ? '0'+ newMinute: newMinute}`,
-                    coord: (li.offsetTop + (li.offsetHeight / 2)) + fiveMinuteCoord * x
-                })
-            }
+        }
+        else{
+            create3MinuteBefore(li, hour, minute, fiveMinuteCoord)
+            coords.push({
+                time: `${hour < 10 ? '0'+ hour: hour}:${minute < 10 ? '0'+ minute: minute}`,
+                coord: (li.offsetTop + (li.offsetHeight / 2))
+            })
+            create2MinuteAfter(li, hour, minute, fiveMinuteCoord)
         }
     })
     return coords
