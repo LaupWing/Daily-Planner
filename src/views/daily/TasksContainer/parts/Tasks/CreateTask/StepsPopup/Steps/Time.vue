b9e36d17594f863429e630106f58e8c2ae2db102
LaupWing <loc_strict@hotmail.com>
2020-07-02 19:49:11 +0200

    modualize the steps

---
+++ src/views/daily/TasksContainer/parts/Tasks/CreateTask/StepsPopup/Steps/Time.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="step">
+        <div class="starting">
+                <input 
+                    type="number" 
+                    name="hours" 
+                    min="00" 
+                    max="23" 
+                    v-model="starting.hour"
+                >
+                <input 
+                    type="number" 
+                    name="hours" 
+                    min="00" 
+                    max="23" 
+                    v-model="starting.minute"
+                >
+            </div>
+            <div class="end">
+                <input 
+                    type="number" 
+                    name="hours" 
+                    min="00" 
+                    max="23" 
+                    v-model="ending.hour"
+                >
+                <input 
+                    type="number" 
+                    name="hours" 
+                    min="00" 
+                    max="23" 
+                    v-model="ending.minute"
+                >
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Time',
+    props:{
+        starting:{
+            type: Object,
+            required: true
+        },
+        ending:{
+            type: Object,
+            required: true
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
