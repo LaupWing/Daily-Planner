4ab3f2a1f8eb0cc52302925bac56d08ab0449d1f
LaupWing <loc_strict@hotmail.com>
2021-01-08 14:27:54 +0100

    just some minor changes

--- src/store/modules/tasks.js
+++ src/store/modules/tasks.js
@@ -2,87 +2,87 @@ import firebase from 'firebase'
 const db = firebase.firestore()
 
 const state = {
-    userData:{},
-    colorLabels: null,
-    dailyTasks: null,
-    creatingTask: false
+   userData: {},
+   colorLabels: null,
+   dailyTasks: null,
+   creatingTask: false
 }
 
 const getters = {
-    getUserData: state => state.userData,
-    colorLabels: state => state.colorLabels,
-    dailyTasks: state => state.dailyTasks,
-    creatingTask: state => state.creatingTask
+   getUserData: state => state.userData,
+   colorLabels: state => state.colorLabels,
+   dailyTasks: state => state.dailyTasks,
+   creatingTask: state => state.creatingTask
 }
 
 const actions = {
-    plannerWatcher({rootGetters, commit}){
-        db
-            .collection('planner')
-            .doc(rootGetters.user.uid)
-            .onSnapshot(snapshot=>{
-                if(snapshot.exists){
-                    commit('setUserData', {...snapshot.data()})
-                }
-            })
-    },
-    async updateColor({commit, rootGetters}, updatedColorLabels){
-        await db
-            .collection('planner')
-            .doc(rootGetters.user.uid)
-            .update({
-                colorLabels: updatedColorLabels
-            }) 
-        commit('updateLabels', updatedColorLabels)
-    },
-    updateTask({commit, rootGetters}, updatedTask){
-        return db
-                .collection('planner')
-                .doc(rootGetters.user.uid)
-                .update({
-                    dailyTasks: updatedTask
-                })
-                .then(()=>{
-                    commit('updateTasks', updatedTask)
-                })
-    },
-    addNewTask({commit, rootGetters}, addedTaskToDailyTasks){
-        return db
-            .collection('planner')
-            .doc(rootGetters.user.uid)
-            .update({
-                dailyTasks: addedTaskToDailyTasks
-            })
-            .then(()=>{
-                commit('updateTasks', addedTaskToDailyTasks)
-            })
-            .catch(()=>{
-                return db
-                    .collection('planner')
-                    .doc(rootGetters.user.uid)
-                    .set({
-                        dailyTasks: addedTaskToDailyTasks
-                    })
-                    .then(()=>{
-                        commit('updateTasks', addedTaskToDailyTasks)
-                    })
-            })
-    }
+   plannerWatcher({ rootGetters, commit }) {
+      db
+         .collection('planner')
+         .doc(rootGetters.user.uid)
+         .onSnapshot(snapshot => {
+            if (snapshot.exists) {
+               commit('setUserData', { ...snapshot.data() })
+            }
+         })
+   },
+   async updateColor({ commit, rootGetters }, updatedColorLabels) {
+      await db
+         .collection('planner')
+         .doc(rootGetters.user.uid)
+         .update({
+            colorLabels: updatedColorLabels
+         })
+      commit('updateLabels', updatedColorLabels)
+   },
+   updateTask({ commit, rootGetters }, updatedTask) {
+      return db
+         .collection('planner')
+         .doc(rootGetters.user.uid)
+         .update({
+            dailyTasks: updatedTask
+         })
+         .then(() => {
+            commit('updateTasks', updatedTask)
+         })
+   },
+   addNewTask({ commit, rootGetters }, addedTaskToDailyTasks) {
+      return db
+         .collection('planner')
+         .doc(rootGetters.user.uid)
+         .update({
+            dailyTasks: addedTaskToDailyTasks
+         })
+         .then(() => {
+            commit('updateTasks', addedTaskToDailyTasks)
+         })
+         .catch(() => {
+            return db
+               .collection('planner')
+               .doc(rootGetters.user.uid)
+               .set({
+                  dailyTasks: addedTaskToDailyTasks
+               })
+               .then(() => {
+                  commit('updateTasks', addedTaskToDailyTasks)
+               })
+         })
+   }
 }
 
 const mutations = {
-    setUserData: (state, {colorLabels, dailyTasks})=> {
-        state.colorLabels = colorLabels
-        state.dailyTasks = dailyTasks
-    },
-    updateTasks: (state,updatedTasks)=>(state.userData.dailyTasks = updatedTasks),
-    updateLabels: (state,updatedColorLabels)=>(state.userData.colorLabels = updatedColorLabels),
-    setCreatingTask: (state, onOrOff)=> state.creatingTask = onOrOff
+   setUserData: (state, { colorLabels, dailyTasks }) => {
+      state.colorLabels = colorLabels
+      state.dailyTasks = dailyTasks
+   },
+   updateTasks: (state, updatedTasks) => (state.userData.dailyTasks = updatedTasks),
+   updateLabels: (state, updatedColorLabels) => (state.userData.colorLabels = updatedColorLabels),
+   setCreatingTask: (state, onOrOff) => state.creatingTask = onOrOff
 }
 
 export default {
-    state,
-    getters,
-    actions,
-    mutations
+   state,
+   getters,
+   actions,
+   mutations
 }
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,105 +1,105 @@
 import firebase from 'firebase'
 
 const state = {
-    user: null,
-    userData: null,
-    geolocation:{
-        lat: 52.370216,
-        lng: 4.895168
-    },
+   user: null,
+   userData: null,
+   geolocation: {
+      lat: 52.370216,
+      lng: 4.895168
+   },
 }
 
 const getters = {
-    user: state => state.user,
-    userData: state => state.userData,
-    geolocation: state => state.geolocation,
+   user: state => state.user,
+   userData: state => state.userData,
+   geolocation: state => state.geolocation,
 }
 
 const actions = {
-    userWatcher({dispatch}){
-        firebase.auth().onAuthStateChanged(()=>{
-            dispatch('checkUser')
-        })
-    },
-    checkUser({commit, dispatch}){
-        const user = firebase.auth().currentUser
-        commit('setUser', user)
-        if(user){
-            dispatch('updateUser', {
-                type: 'geolocation',
-                value:{
-                    lat: state.geolocation.lat,
-                    lng: state.geolocation.lng
-                }
+   userWatcher({ dispatch }) {
+      firebase.auth().onAuthStateChanged(() => {
+         dispatch('checkUser')
+      })
+   },
+   checkUser({ commit, dispatch }) {
+      const user = firebase.auth().currentUser
+      commit('setUser', user)
+      if (user) {
+         dispatch('updateUser', {
+            type: 'geolocation',
+            value: {
+               lat: state.geolocation.lat,
+               lng: state.geolocation.lng
+            }
+         })
+         dispatch('plannerWatcher')
+      }
+   },
+   // eslint-disable-next-line
+   updateUser({ commit }, { type, value }) {
+      // firebase
+      //     .firestore()
+      //     .collection('users')
+      //     .doc(state.user.uid)
+      //     .update({
+      //         [type]: value
+      //     })
+      //     .then((userData)=>{
+      //         commit('setUserData', userData)
+      //     })
+   },
+   setLocation({ commit }) {
+      if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(pos => {
+            commit('setGeoLocation', {
+               lat: pos.coords.latitude,
+               lng: pos.coords.longitude
             })
-            dispatch('plannerWatcher')
-        }
-    },
-    // eslint-disable-next-line
-    updateUser({commit}, {type, value}){
-        // firebase
-        //     .firestore()
-        //     .collection('users')
-        //     .doc(state.user.uid)
-        //     .update({
-        //         [type]: value
-        //     })
-        //     .then((userData)=>{
-        //         commit('setUserData', userData)
-        //     })
-    },
-    setLocation({commit}){
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(pos=>{
-                commit('setGeoLocation', {
-                    lat: pos.coords.latitude,
-                    lng: pos.coords.longitude
-                })
-            })
-        }
-    },
-    // eslint-disable-next-line
-    registerUser({commit}, {email, password, alias}){
-        return firebase
-            .auth()
-            .createUserWithEmailAndPassword(email, password)
-            .then(cred=>{
-                const user = cred.user
-                return firebase
-                    .firestore()
-                    .doc(user.uid)
-                    .set({
-                        alias: alias,
-                        geolocation: null
-                    })
-            })
-            .catch(err=>{
-                throw err.message
-            })
-    },
-    // eslint-disable-next-line
-    loginUser({commit}, {email, password}){
-        return firebase
-            .auth()
-            .signInWithEmailAndPassword(email, password)
-            .then((user)=>{
-                return user
-            })
-            .catch(err=>{
-                throw err.message
-            })
-    }
+         })
+      }
+   },
+   // eslint-disable-next-line
+   registerUser({ commit }, { email, password, alias }) {
+      return firebase
+         .auth()
+         .createUserWithEmailAndPassword(email, password)
+         .then(cred => {
+            const user = cred.user
+            return firebase
+               .firestore()
+               .doc(user.uid)
+               .set({
+                  alias: alias,
+                  geolocation: null
+               })
+         })
+         .catch(err => {
+            throw err.message
+         })
+   },
+   // eslint-disable-next-line
+   loginUser({ commit }, { email, password }) {
+      return firebase
+         .auth()
+         .signInWithEmailAndPassword(email, password)
+         .then((user) => {
+            return user
+         })
+         .catch(err => {
+            throw err.message
+         })
+   }
 }
 
 const mutations = {
-    setUser: (state,user) => (state.user = user),
-    setUserData: (state,userData) => (state.userData = userData),
-    setGeoLocation: (state,{lat,lng}) => (state.geolocation = {lat,lng}),
+   setUser: (state, user) => (state.user = user),
+   setUserData: (state, userData) => (state.userData = userData),
+   setGeoLocation: (state, { lat, lng }) => (state.geolocation = { lat, lng }),
 }
 
 export default {
-    state,
-    getters,
-    actions,
-    mutations
+   state,
+   getters,
+   actions,
+   mutations
 }
